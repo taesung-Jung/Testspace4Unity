d5af394673c22a0581e4a4b88099c8bb8c234d2f
yang4rung <yang4rung@gmail.com>
2025-07-27 15:41:54 +0900

    combine

--- Controlproject/Assets/WeaponDamage.cs
+++ Controlproject/Assets/WeaponDamage.cs
@@ -3,13 +3,13 @@ using UnityEngine;
 public class WeaponDamage : MonoBehaviour
 {
     public int damage = 10;
-    public string playerTag = "Player"; // 플레이어 오브젝트의 Tag
+    public string playerTag = "Player";
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag(playerTag))
         {
-            // PlayerHealth 스크립트에 데미지 적용
+            // PlayerHealth
             PlayerHealth hp = other.GetComponent<PlayerHealth>();
             if (hp != null)
             {
