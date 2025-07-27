dbf95362964845e2742cbfe8d3fb10c66ee2a369
TaesungJung <wjdxotjdfgo@gmail.com>
2025-07-27 15:58:22 +0900

    NoDamageV1

--- Controlproject/Assets/PlayerHealth.cs
+++ Controlproject/Assets/PlayerHealth.cs
@@ -5,15 +5,33 @@ public class PlayerHealth : MonoBehaviour
     public int maxHp = 100;
     public int currentHp;
 
+    public float NodamageTime = 5.0f;
+    public float NodamageTimer = 0.0f;
+    public bool isDamaged = false;
+
     void Start() { currentHp = maxHp; }
+
+    void Update()
+    {
+        if (isDamaged)
+        {
+            NodamageTimer -= Time.deltaTime;
+            if (NodamageTimer <= 0f)
+            {
+                isDamaged = false;
+            }
+        }
+    }
 
     public void TakeDamage(int damage)
     {
+        if(isDamaged) return;
         currentHp -= damage;
         if (currentHp < 0) currentHp = 0;
+        isDamaged = true;
     }
 
-    // 0~1 사이의 값 반환
+    // 0~1
     public float CurrentHpNormalized()
     {
         return (float)currentHp / maxHp;
