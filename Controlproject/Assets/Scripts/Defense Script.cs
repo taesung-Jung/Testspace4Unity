6c08bf8659e898903ac29294ea6b53382c310b73
yang4rung <yang4rung@gmail.com>
2025-10-27 19:02:48 +0900

    defense2
    defense

---
+++ Controlproject/Assets/Scripts/Defense Script.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class DefenseScript : MonoBehaviour
+{
+    public bool isAttacking;
+    public bool isDefending;
+    public bool isColliding;
+    public bool broken;
+
+    public float maxStamina;
+    public float stamina;
+    public float staminaRegenRate;
+
+
+    void Update()
+    {   
+
+        //스태미나 회복
+        if (!isDefending && stamina < maxStamina)
+        {
+            stamina += staminaRegenRate * Time.deltaTime;
+        }
+        if (!broken)
+        {
+            //공격 중 방어 불가
+            if (!isAttacking)
+            {
+                isDefending = Input.GetKey(KeyCode.Tab);
+            }
+        }
+        if (stamina > maxStamina)
+        {
+            stamina = maxStamina;
+        }
+
+
+        if (stamina <= 0 && !broken)
+        {
+            StartCoroutine(GuardBreak());
+        }
+    }
+
+        
+    public void UseStamina(float amount)
+    {
+        stamina -= amount;
+        if (stamina < 0)
+            stamina = 0;
+    }
+
+    IEnumerator GuardBreak()
+    {
+        broken = true;
+
+        yield return new WaitForSeconds(3f); // 3초간 방어 불가
+
+        broken = false;
+
+    }
+}
